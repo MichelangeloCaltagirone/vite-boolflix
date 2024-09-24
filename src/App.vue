ad167b1f40fe4262f74ad66cd87775944b7c4183
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-09-24 15:53:52 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import AppMain from './components/AppMain.vue';
+import AppHeader from './components/AppHeader.vue';
+
+</script>
+
+<template>
+
+<AppHeader/>
+<AppMain/>
+
+</template>
+
+<style scoped lang="scss">
+
+@use './style/general.scss' as *;
+
+</style>
